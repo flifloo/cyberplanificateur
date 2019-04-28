1b19e93ffe02d871e420dc2d356100dd231fbfa7
flifloo <flifloo@gmail.com>
2019-04-28 09:17:08 +0200

    Setup basic site and login system

--- site.py
+++ site.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, session, render_template
 import twitter_credentials, tweepy
 
 app = Flask(__name__)
-app.secret_key = "I'm a very mysterious secret key"
+app.secret_key = "*i9uld6u@t!kxl9%o+byxqf14&a&&@y@q=l$!lg4m%b-a*^o(a"
 
 def is_keys(session):
     try:
@@ -26,6 +26,12 @@ def api_valid(session):
     else:
         return True
 
+def is_login(session):
+    if is_keys(session) and api_valid(session):
+        return True
+    else:
+        return False
+
 @app.route("/tlogin")
 def t_login():
     auth = tweepy.OAuthHandler(twitter_credentials.consumer_key, twitter_credentials.consumer_secret_key, "https://cyberplanificateur.flifloo.fr/tlogin")
@@ -38,31 +44,30 @@ def t_login():
         else:
             session["access_token"] = auth.access_token
             session["access_secret_token"] = auth.access_token_secret
-            return "Login !"
-    else:
-        if is_keys(session) and api_valid(session):
-            return "Already login !"
-        else:
-            return redirect(auth.get_authorization_url())
+    elif not is_login(session):
+        return redirect(auth.get_authorization_url())
+    return redirect("/")
 
 @app.route("/tlogout")
 def t_logout():
-    if is_keys(session) and api_valid(session):
+    if is_keys(session):
         session.pop("access_token", None)
         session.pop("access_secret_token", None)
-        return "Logout !"
-    else:
-        return "Not login !"
+    return redirect("/")
 
 
 @app.route("/tpost")
 def t_post():
-    if is_keys(session) and api_valid(session):
+    if is_login(session):
         api = api_login(session)
         api.update_status("bloup")
         return "Send !"
     else:
         return "Not login !"
+
+@app.route("/")
+def home():
+    return render_template("index.html", login = is_login(session))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host="0.0.0.0")
